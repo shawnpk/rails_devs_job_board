e60392f90ff43e4a0e6576794f565d75e14bb0c2
Shawn Kearney <shawnpk29@me.com>
2020-09-01 11:23:03 -0600

    Fix error on generating filename

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def render_svg(name, styles: 'fill-current text-gray-500', title: nil)
-    filename = name.svg.to_s
+    filename = "#{name}.svg"
     title ||= name.underscore.humanize
     inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
   end
